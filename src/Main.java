52b5c0bedafba08efecb0aae3b57ff00a6517f9e
[akif ödül] <[akifodul@hotmail.com]>
2023-04-27 06:41:23 +0300

    .

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,26 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        int r;
+        double 𝛼;
+        double 𝜋 = 3.14;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Scanner input = new Scanner(System.in);
+        System.out.println("Dairenin Yarıçapını Girin: ");
+        r = input.nextInt();
+
+        System.out.println("Merkez Açısının Ölçüsünü Girin: ");
+        𝛼 = input.nextInt();
+
+        double alan = 𝜋 * r * r;
+        double cevre = 2 * 𝜋 * r;
+        double daireDilimininAlani = (𝜋 * (r*r) * 𝛼) / 360;
+
+
+        /* System.out.println("Dairenin Alanı: " + alan);
+        System.out.println("Dairenin Çevresi: " + cevre); */
+        System.out.println("Daire Diliminin Alanı: " + daireDilimininAlani);
+
     }
 }
